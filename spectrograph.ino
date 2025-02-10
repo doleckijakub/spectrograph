25ae80d923fb78561d921d42628151b4aec00c98
Jakub Dolecki <doleckijakub@users.noreply.github.com>
2025-02-10 17:30:52 +0100

    Altered arduino code

--- spectrograph.ino
+++ spectrograph.ino
@@ -20,7 +20,7 @@
 LiquidCrystal lcd(PIN_LCD_RS, PIN_LCD_E, PIN_LCD_D4, PIN_LCD_D5, PIN_LCD_D6, PIN_LCD_D7);
 uint8_t customChar[BOARD_W_CHARS * BOARD_H_CHARS][8] = {0};
 
-void drawPixel(int x, int y, bool on) {
+void setPixel(int x, int y, bool on) {
   int charX = x / 5;
   int charY = y / 8;
   int pixelX = x % 5;
@@ -28,35 +28,41 @@ void drawPixel(int x, int y, bool on) {
   
   if (charX < BOARD_W_CHARS && charY < BOARD_H_CHARS) {
     int charIndex = charX + charY * BOARD_W_CHARS;
-    uint8_t currentState = customChar[charIndex][pixelY] & (1 << (4 - pixelX));
-    
-    if ((on && currentState) || (!on && !currentState)) return;
-    
-    if (on) customChar[charIndex][pixelY] |= (1 << (4 - pixelX));
-    else customChar[charIndex][pixelY] &= ~(1 << (4 - pixelX));
-
-    lcd.createChar(charIndex, customChar[charIndex]);
-    lcd.setCursor(charX, charY);
-    lcd.write(charIndex);
+    if (on) {
+      customChar[charIndex][pixelY] |= (1 << (4 - pixelX));
+    } else {
+      customChar[charIndex][pixelY] &= ~(1 << (4 - pixelX));
+    }
   }
 }
 
 void drawGraph(float values[]) {
-  for (int x = 1; x < BOARD_W - 1; x++)
-    for (int y = 1; y < BOARD_H - 1; y++)
-      drawPixel(x, y, false);
+  memset(customChar, 0, sizeof(customChar));
   
   for (int x = 0; x < BOARD_W; x++) {
-    drawPixel(x, 0, true);
-    drawPixel(x, BOARD_H - 1, true);
-    
-    if (1 <= x && x <= 19 && 0.f <= values[x - 1] && values[x - 1] <= 1.f)
-      drawPixel(x, 1 + int((1.f - values[x - 1]) * (BOARD_H - 2)), true);
+    setPixel(x, 0, true);
+    setPixel(x, BOARD_H - 1, true);
   }
 
   for (int y = 0; y < BOARD_H; y++) {
-    drawPixel(0, y, true);
-    drawPixel(BOARD_W - 1, y, true);
+    setPixel(0, y, true);
+    setPixel(BOARD_W - 1, y, true);
+  }
+
+  for (int x = 1; x <= 19; x++) {
+    if (0.f <= values[x - 1] && values[x - 1] <= 1.f) {
+      int y = 1 + int((1.f - values[x - 1]) * (BOARD_H - 2));
+      setPixel(x, y, true);
+    }
+  }
+
+  for (int charY = 0; charY < BOARD_H_CHARS; charY++) {
+    for (int charX = 0; charX < BOARD_W_CHARS; charX++) {
+      int charIndex = charX + charY * BOARD_W_CHARS;
+      lcd.createChar(charIndex, customChar[charIndex]);
+      lcd.setCursor(charX, charY);
+      lcd.write(charIndex);
+    }
   }
 }
 
@@ -81,5 +87,5 @@ void loop() {
   lcd.setCursor(5, 0);
   lcd.print(values[0]);
 
-  delay(200);
+  delay(50);
 }
